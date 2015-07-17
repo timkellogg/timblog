5ce9c672ef0e1e916cb4fa07640ee6792a1a1ca9
timkellogg <tim.kellogg@gmail.com>
2015-07-17 16:50:28 -0700

    finish first draft of site

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
 	
 	resources :articles 
 
-  match '/help',    to: 'static_pages#help',    via: 'get'  
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/resume',  to: 'static_pages#resume',  via: 'get'
 end
